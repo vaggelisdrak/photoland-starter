85c2a6ae8ceb84297d04bd9f6c491def04973c92
vaggelisdrak <v.drakontaeidis@gmail.com>
2024-08-18 18:47:05 +0300

    fixed images bug

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -14,7 +14,7 @@ const CartItem = ({ item }) => {
     <div className='flex gap-x-8'>
       <Link to={`product/${item.id}`} className='w-[70px] h-[70px]'>
         <img
-          src={`http://localhost:1337${item.attributes.image.data.attributes.url}`}
+          src={`${item.attributes.image.data.attributes.url}`}
           alt=''
         />
       </Link>
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -25,7 +25,7 @@ const Product = ({ product }) => {
         <div className='w-full h-[200px] flex items-center justify-center relative'>
           <img
             className='w-[160px] h-[160px] group-hover:scale-90 transition-all '
-            src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
+            src={`${product.attributes.image.data.attributes.url}`}
             alt='product-image'
           />
         </div>
