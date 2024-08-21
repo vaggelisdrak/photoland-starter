5135c9adb43c332e20467fa77eb587ffd633185d
vaggelisdrak <v.drakontaeidis@gmail.com>
2024-08-22 01:06:31 +0300

    fixed subsubcategories

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
-// useParams hook
 import { useParams, useSearchParams } from 'react-router-dom';
-// useFetch hook
 import useFetch from '../hooks/useFetch';
-// components
 import CategoryNav from '../components/CategoryNav';
 import Product from '../components/Product';
 import NoProducts from '../img/Untitled design (57).png';
@@ -11,51 +8,44 @@ import { Oval } from 'react-loader-spinner';
 
 const Products = () => {
   const { id } = useParams();
-  //get subcategory from url search params
   const [searchParams, setSearchParams] = useSearchParams();
-  console.log('searchParams',searchParams.get('subcategory'))
-    
-  // State for filters
   const [priceRange, setPriceRange] = useState([0, 5000]);
   const [sortOption, setSortOption] = useState('');
-  const [selectedSubcategory, setSelectedSubcategory] = useState(searchParams.get('subcategory'));
+  const [selectedSubcategory, setSelectedSubcategory] = useState(searchParams.get('subcategory') || '');
+  const [selectedSubsubcategory, setSelectedSubsubcategory] = useState('');
+  const [subsubcategories, setSubsubcategories] = useState([]);
+  const [title, setTitle] = useState(null);
 
-  console.log('selected sub',selectedSubcategory)
-  
+  console.log('id', id);
+
   // Fetch products based on category id, filters, sorting, and subcategory
-  const { data } = useFetch(
+  const { data: productsData } = useFetch(
     `/products?populate=*&filters[categories][id][$eq]=${id}` +
     `&filters[price][$gte]=${priceRange[0]}&filters[price][$lte]=${priceRange[1]}` +
     `${selectedSubcategory ? `&filters[subcategories][id][$eq]=${selectedSubcategory}` : ''}` +
+    `${selectedSubsubcategory ? `&filters[subsubcategories][id][$eq]=${selectedSubsubcategory}` : ''}` +
     `${sortOption ? `&sort=${sortOption}` : ''}`
   );
 
-  // State for category title and subcategories
-  const [title, setTitle] = useState(null);
-  const [subcategories, setSubcategories] = useState([]);
-  
-  // Fetch sub sub category details
-  //const { data: categoryData } = useFetch(`/subcategories/${selectedSubcategory}?populate=subsubcategories`);
-  const { subsubcategoriesData } = useFetch(`/subsubcategories`);
-  console.log('categoryDataaa',subsubcategoriesData)
+  // Fetch subsubcategories based on selected subcategory
+  const { data: subSubCategoryData } = useFetch(`/subcategories/${selectedSubcategory}?populate=subsubcategories`);
 
-  // Reset selectedSubcategory when id changes
   useEffect(() => {
-    setSelectedSubcategory('');
-  }, [id]);
-  
-  // Set the title and subcategories when main product data is fetched
-  useEffect(() => {
-    if (data && data.length > 0) {
-      setTitle(data[0].attributes.categories.data[0].attributes.title);
+    if (subSubCategoryData) {
+      const subsubcategories = subSubCategoryData?.attributes?.subsubcategories?.data || [];
+      setSubsubcategories(subsubcategories);
     }
-  }, [data]);
+  }, [subSubCategoryData]);
 
-  /*useEffect(() => {
-    if (categoryData && categoryData.attributes.subcategories.data) {
-      setSubcategories(categoryData.attributes.subcategories.data);
+  useEffect(() => {
+    setSelectedSubcategory(searchParams.get('subcategory') || '');
+  }, [searchParams]);
+
+  useEffect(() => {
+    if (productsData && productsData.length > 0) {
+      setTitle(productsData[0].attributes.categories.data[0].attributes.title);
     }
-  }, [categoryData]);*/
+  }, [productsData]);
 
   // Handle price range change
   const handlePriceRangeChange = (event) => {
@@ -70,24 +60,24 @@ const Products = () => {
     setSortOption(event.target.value);
   };
 
-  // Handle subcategory change
-  const handleSubcategoryChange = (event) => {
-    setSelectedSubcategory(event.target.value);
+  const handleSubsubcategoryChange = (event) => {
+    const newSubsubcategory = event.target.value;
+    setSelectedSubsubcategory(newSubsubcategory);
   };
 
-  if (!data) {
-   // return <div className='container mx-auto'>loading...</div>;
-   return <div className='container mx-auto flex space-x-3 text-primary'>
-    <h2 className="text-[30px]">loading...</h2>
-    <Oval
-    visible={true}
-    height="40"
-    width="40"
-    color="#000000"
-    ariaLabel="oval-loading"
-    wrapperStyle={{}}
-    wrapperClass=""
-    /></div>
+  if (!productsData) {
+    return (
+      <div className='container mx-auto flex space-x-3 text-primary'>
+        <h2 className="text-[30px]">loading...</h2>
+        <Oval
+          visible={true}
+          height="40"
+          width="40"
+          color="#000000"
+          ariaLabel="oval-loading"
+        />
+      </div>
+    );
   }
 
   return (
@@ -101,18 +91,7 @@ const Products = () => {
               {title}
             </div>
 
-            {/* Subcategories 
-            {subcategories.length > 0 && (
-              <div className='my-6'>
-                <div className='flex flex-wrap gap-4'>
-                  {subcategories.map((subcategory) => (
-                    <div key={subcategory.id} className='bg-blue-800 p-2 rounded-lg text-sm'>
-                      {subcategory.attributes.title}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}*/}
+            
             
             {/* Filter and Sorting Section */}
             <div className='flex flex-col md:flex-row justify-between mb-6'>
@@ -139,29 +118,28 @@ const Products = () => {
                   />
                 </label>
               </div>
-              {/* Subcategory Filter 
+              {/* Subsubcategories Dropdown */}
               <div>
-              {subcategories.length > 0 && (
-                <div>
+              {subsubcategories?.length > 0 && (
+                <div className='py-1'>
                   <label className='text-primary'>
                     Υποκατηγορία:
                     <select 
-                      value={selectedSubcategory} 
-                      onChange={handleSubcategoryChange}
-                      className='border rounded ml-2 px-1 py-1 text-primary mt-5 sm:mt-0'
+                      value={selectedSubsubcategory} 
+                      onChange={handleSubsubcategoryChange}
+                      className='border rounded ml-2 px-1 py-1 text-primary w-[150px] truncate'
                     >
                       <option value=''>OΛΑ</option>
-                      {subcategories.map((subcategory) => (
-                        <option key={subcategory.id} value={subcategory.id}>
-                          {subcategory.attributes.title}
+                      {subsubcategories.map((subsubcategory) => (
+                        <option key={subsubcategory.id} value={subsubcategory.id} className='truncate'>
+                          {subsubcategory?.attributes?.title}
                         </option>
                       ))}
                     </select>
                   </label>
                 </div>
               )}
-              </div>*/}
-
+              </div>
               {/* Sorting Options */}
               <div>
                 <label className='text-primary'>
@@ -176,10 +154,11 @@ const Products = () => {
                 </label>
               </div>
             </div>
+            <hr/>
             {/* Product Grid or No Products Message */}
-            {data && data.length > 0 ? (
+            {productsData && productsData.length > 0 ? (
               <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-[15px] md:gap-[30px]'>
-                {data.map((product) => (
+                {productsData.map((product) => (
                   <Product product={product} key={product.id} />
                 ))}
               </div>
